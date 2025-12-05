f3a783db5d44eb011368daa320f2dc220b1896d8
Navraj Kalsi <navrajkalsi@icloud.com>
2025-12-04 22:23:58 -0500

    day 4

--- day4/printing_dept_1.py
+++ day4/printing_dept_1.py
@@ -4,6 +4,8 @@ adjacent_pos = 8
 input_grid = []
 answer_grid = []
 
+debug = False
+
 def print_grid(grid):
     for line in grid:
         print(line)
@@ -11,18 +13,51 @@ def print_grid(grid):
 def accessible(row_num, col_num):
     adjacent_rolls = 0
     for row in range(-1, 2):
-        if row_num + row < 0 or row_num + row > len(input_grid):
+        if row_num + row < 0 or row_num + row >= len(input_grid):
             continue
-        for col in range(-1, 2):
-            if col_num + col < 0 or col_num + col > len(input_grid[0]):
-                continue
 
-with open('demo.txt', 'r') as input:
-    for line in input:
-        input_grid.append(input.readline().strip())
+        for col in range(-1, 2):
+            if row == 0 and col == 0:
+                continue
+            if col_num + col < 0 or col_num + col >= len(input_grid[0]):
+                continue
+            if input_grid[row_num + row][col_num + col] == '@':
+                adjacent_rolls += 1
+            if adjacent_rolls > max_rolls:
+                break
+
+        if adjacent_rolls > max_rolls:
+            break
+
+    return adjacent_rolls <= max_rolls
+
+with open('input.txt', 'r') as input:
+    input_arr = input.readlines()
+    for line in input_arr:
+        input_grid.append(line.strip())
 
 for row_num, rolls in enumerate(input_grid):
+    answer_row = []
     for col_num, roll in enumerate(rolls):
-        print(f"row_num: {row_num}, col_num: {col_num}, roll: {roll}")
+        if debug:
+            print(f"row_num: {row_num}, col_num: {col_num}, roll: {roll}")
 
-print_grid(input_grid)
+        if roll != '@':
+            answer_row.append(roll)
+        elif accessible(row_num, col_num):
+            answer += 1
+            answer_row.append('X')
+        else:
+            answer_row.append('@')
+
+    answer_row_str = ""
+    for roll in answer_row:
+        answer_row_str += roll
+    answer_grid.append(answer_row_str)
+
+    if debug:
+        print(f"Answer row: {answer_row_str}")
+
+print_grid(answer_grid)
+print()
+print(answer)
