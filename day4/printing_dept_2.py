1464aaffd2237e2c62e7f009168ea2344cb6e3c0
Navraj Kalsi <navrajkalsi@icloud.com>
2025-12-04 22:59:08 -0500

    day 4

---
+++ day4/printing_dept_2.py
@@ -0,0 +1,82 @@
+answer = 0
+max_rolls = 3
+adjacent_pos = 8
+input_grid = []
+answer_grid = []
+
+debug = False
+
+def print_grid(grid):
+    for line in grid:
+        print(line)
+
+def accessible(row_num, col_num):
+    adjacent_rolls = 0
+    for row in range(-1, 2):
+        if row_num + row < 0 or row_num + row >= len(input_grid):
+            continue
+
+        for col in range(-1, 2):
+            if row == 0 and col == 0:
+                continue
+            if col_num + col < 0 or col_num + col >= len(input_grid[0]):
+                continue
+            if input_grid[row_num + row][col_num + col] == '@':
+                adjacent_rolls += 1
+            if adjacent_rolls > max_rolls:
+                break
+
+        if adjacent_rolls > max_rolls:
+            break
+
+    return adjacent_rolls <= max_rolls
+
+with open('demo.txt', 'r') as input:
+    input_arr = input.readlines()
+    for line in input_arr:
+        input_grid.append(line.strip())
+
+iteration = 0
+while True:
+    iteration += 1
+    print(f"\nIteration: {iteration}")
+
+    answer_grid = []
+    found = False
+
+    for row_num, rolls in enumerate(input_grid):
+        answer_row = []
+        for col_num, roll in enumerate(rolls):
+            if debug:
+                print(f"row_num: {row_num}, col_num: {col_num}, roll: {roll}")
+
+            if roll != '@':
+                answer_row.append(roll)
+            elif accessible(row_num, col_num):
+                answer += 1
+                answer_row.append('X')
+                found = True
+            else:
+                answer_row.append('@')
+
+        answer_row_str = ""
+        for roll in answer_row:
+            answer_row_str += roll
+        answer_grid.append(answer_row_str)
+
+        if debug:
+            print(f"Answer row: {answer_row_str}")
+
+    if found == False:
+        break
+
+    print_grid(answer_grid)
+
+    for index, line in enumerate(answer_grid):
+        answer_grid[index] = line.replace('X', '.')
+
+    input_grid = answer_grid
+
+print_grid(answer_grid)
+print()
+print(answer)
