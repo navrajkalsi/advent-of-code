0aa9526335860326b108325f244b4e55371f2ba7
Navraj Kalsi <navrajkalsi@icloud.com>
2025-12-06 13:46:22 -0500

    day 6

--- day6/trash_compactor_2.py
+++ day6/trash_compactor_2.py
@@ -3,54 +3,49 @@ answer = 0
 debug = False
 
 numbers = []
-operators = []
+operators = ""
 
 with open('input.txt', 'r') as input:
-    lines = [line.strip().split() for line in input.readlines()]
+    lines = [line.replace('\n', '') for line in input.readlines()]
 
     numbers.extend(lines[:-1])
     operators = lines[-1]
 
 if debug:
-    print(f"Numbers: {numbers}\n")
-    print(f"Operators: {operators}\n")
+    print("Numbers:")
+    for num in numbers:
+        print(num)
+    print(f"\nOperators:\n{operators}\n")
 
-for index, operator in enumerate(operators):
+empty_cols = [] # space col indices between problems
+for index in range(len(operators)):
+    operator = operators[index]
+    if (operator == '*' or operator == '+') and index != 0:
+        empty_cols.append(index - 1)
+
+longest_line = len(max(numbers, key=len))
+
+operator = operators[0]
+sub_answer = 0 if operator == '+' else 1
+for index in range(longest_line + 1):
+    if index in empty_cols:
+        answer += sub_answer
+        operator = operators[index + 1]
+        sub_answer = 0 if operator == '+' else 1
+        continue
+    if index == longest_line:
+        answer += sub_answer
+        break
+
+    num = ""
+    for line in numbers:
+        if len(line) <= index:
+            continue
+        num += line[index]
+    num = int(num)
     if operator == '*':
-        product = 1
-        max_len = 0
-
-        for line in numbers: # calculate max len
-            if max_len < len(line[index]):
-                max_len = len(line[index])
-
-        for place in range(max_len): # nums will be inverted
-            num = ""
-            for line in numbers: # performing operation
-                line = [num[::-1] for num in line]
-                if len(line[index]) < place + 1:
-                    continue
-                num += line[index][place]
-            product *= int(num)
-        print(product)
-        answer += product
-
+        sub_answer *= num
     elif operator == '+':
-        sum = 0
-        max_len = 0
-
-        for line in numbers:
-            if max_len < len(line[index]):
-                max_len = len(line[index])
-
-        for place in range(max_len - 1, -1, -1):
-            num = ""
-            for line in numbers:
-                if len(line[index]) < place + 1:
-                    continue
-                num += line[index][place]
-            sum += int(num)
-        print(sum)
-        answer += sum
+        sub_answer += num
 
 print(answer)
